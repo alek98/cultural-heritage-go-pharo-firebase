3ee8ffe267169bc5704304932ef56d1ea2fe25c3
alekalek <alek@spectra-alarm.com>
2021-04-28 21:19:37 +0200

    updated comment how to run

--- backend/src/alek/main/main.go
+++ backend/src/alek/main/main.go
@@ -5,12 +5,19 @@ import "fmt"
 func main() {
 	fmt.Println("Starting Backend")
 	/*
-		1. cd src/alek/main
-		2. open powershell (not stupid cmd)
-		3. run `firebase emulators:start`
-		3. run `$env:FIRESTORE_EMULATOR_HOST="localhost:8080"`
-		4. run `go run .`
-		5. start coding
+			How to run firebase
+			1. cd firebase
+			2. run `firebase emulators:start`
+
+		  How to run golang
+			-----------------
+			1. cd src/alek/main
+			2. open powershell (not stupid cmd)
+			3. run `$env:FIRESTORE_EMULATOR_HOST="localhost:8080"`
+			4. run `go run .`
+			5. start coding
+
+
 	*/
 	// startServer()
 
