aa2bd54f11e1e490183e6b1aa96af3480cf4fc43
alekalek <alek@spectra-alarm.com>
2021-05-03 19:28:57 +0200

    added likes and dislikes of chs

--- backend/src/alek/controller/chController.go
+++ backend/src/alek/controller/chController.go
@@ -49,8 +49,8 @@ func (*ChController) GetAll(w http.ResponseWriter, request *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(chs)
-
 }
+
 func (*ChController) Search(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -70,4 +70,46 @@ func (*ChController) Search(w http.ResponseWriter, request *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(chs)
+}
+
+func (*ChController) Like(w http.ResponseWriter, request *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var ch model.Ch
+
+	err := json.NewDecoder(request.Body).Decode(&ch)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode("Error unmarshalling object")
+		return
+	}
+
+	result, err := myservice.Like(&ch)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(result)
+}
+
+func (*ChController) Disike(w http.ResponseWriter, request *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var ch model.Ch
+
+	err := json.NewDecoder(request.Body).Decode(&ch)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode("Error unmarshalling object")
+		return
+	}
+
+	result, err := myservice.Disike(&ch)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(result)
 }
